3a8b22465ffd9d8ea0a4ab3fd19b25c73da63c4c
Nicolas <nicontrerasc8@gmail.com>
2022-12-07 10:38:50 -0500

    xd

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,24 @@ import {FaInstagram, FaFacebook, FaWhatsapp} from "react-icons/fa"
 
 const Footer = () => {
   return <footer>
-    <a className='facebook' href='https://www.facebook.com/' target={"_blank"}><FaFacebook/></a>
-    <a className='instagram' href='https://www.instagram.com/' target={"_blank"}><FaInstagram/></a>
-    <a className='whatsapp' href={`https://api.whatsapp.com/send?phone=51949161510&text="¡Hola! vengo de su página web."`} target={"_blank"}><FaWhatsapp/></a>
+    <a className='facebook' 
+      href='https://www.facebook.com/' 
+      target={"_blank"} 
+      rel="noreferrer" >
+        <FaFacebook/>
+    </a>
+    <a className='instagram' 
+        href='https://www.instagram.com/' 
+        target={"_blank"} 
+        rel="noreferrer" >
+          <FaInstagram/>
+      </a>
+    <a className='whatsapp' 
+    href={`https://api.whatsapp.com/send?phone=51949161510&text="¡Hola! vengo de su página web."`} 
+    target={"_blank"} 
+    rel="noreferrer">
+      <FaWhatsapp/>
+    </a>
   </footer>
 }
 
