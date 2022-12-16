3c94bf2f77b4e9c1533e7f924165ebaeed86e0b0
Nicolas <nicontrerasc8@gmail.com>
2022-12-15 21:53:56 -0500

    xd

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ import Carta from "./carta";
 export default function Home() {
   return <>
     <Slider/>
-    <div className="custom-shape-divider-top-1670423651">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-        </svg>
-    </div>
     <Carta/>
   </>
 }
